f0728e30a4325d77e2a6e2c2ad69c770f9a32523
Nicolas Rizk <nrizk6@gmail.com>
2015-02-03 18:08:22 -0500

    Update MyApp.m

--- deployOnDayOne/MyApp.m
+++ deployOnDayOne/MyApp.m
@@ -16,23 +16,25 @@
 @implementation MyApp
 
 -(void)execute {
-//  [self questionList];
-    NSArray *genericQuestions = @[@"Where are you?", @"Whats your fav color?", @"Whats your fav animal?"];
-    NSMutableArray *questions = [NSMutableArray arrayWithArray:genericQuestions];
-    self.motherDict = [[NSMutableDictionary alloc] init];
-    self.questionsAndAnswers = [[NSMutableDictionary alloc] init];
-    [self login];
-    [self menu];
-    }
-
--(void)login {
     
-    NSLog(@"Log in");
+    self.motherDict = [[NSMutableDictionary alloc] init];
+    self.currentUser = [[NSString alloc] init];
+    self.questions = [[NSMutableArray alloc] initWithArray:@[@"Where are you?", @"Whats your fav color?", @"Whats your fav animal?"]];
+  
+    [self login];
 
-    NSString *name = [self requestKeyboardInput];
-        if (!self.motherDict[name]) {
-            [self.motherDict setObject:self.questionsAndAnswers forKey:name];
-        }
+}
+
+
+-(NSString *)login {
+
+    NSLog(@"Please enter your full name to log in");
+    self.currentUser = [self requestKeyboardInput];
+    
+
+    [self menu];
+    return self.currentUser;
+    
 }
 
 -(void)logout {
@@ -41,6 +43,7 @@
 }
 
 -(NSString *)menu {
+    NSLog(@"dictionary ..... %@", self.motherDict);
     NSLog(@"Please choose from the following four options:\r\r1. Be interviewed.\r2. Write a new interview question.\r3. Read an interview with another student.\r4. Logout\r\rSimply type in the option number you are interested in, and press enter.");
     NSInteger menuChoice = [[self requestKeyboardInput] intValue];
     switch (menuChoice) {
@@ -64,27 +67,34 @@
     return [self requestKeyboardInput];
 }
 
-//-(NSMutableArray *)questionList {
-//    NSArray *genericQuestions = @[@"Where are you?", @"Whats your fav color?", @"Whats your fav animal?"];
-//    NSMutableArray *questions = [NSMutableArray arrayWithArray:genericQuestions];
-//    return self.questions;
-//}
-
 -(void)optionOne {
+    if (!self.motherDict[self.currentUser]) {
+        NSMutableDictionary *questionsAndAnswers = [[NSMutableDictionary alloc] init];
+        self.motherDict[self.currentUser] = questionsAndAnswers;
+        [self.motherDict setObject:questionsAndAnswers forKey:self.currentUser];
+    }
     for (NSString *question in self.questions) {
         NSLog(@"%@", question);
-        NSString *response = [self requestKeyboardInput];
-        [self.questionsAndAnswers setObject:response forKey:question];
+        self.response = [self requestKeyboardInput];
+        NSMutableDictionary *currentUserDict = self.motherDict[self.currentUser];
+        [currentUserDict setObject:self.response forKey:question];
+        
     }
+
     [self menu];
 }
+
+
 -(void)optionTwo {
         NSLog(@"What question would you like to add?");
         NSString *newQuestion = [self requestKeyboardInput];
         [self.questions addObject:newQuestion];
         NSLog(@"Thanks! We've added your question to our interview");
-        NSLog(@"%@", self.questions);
+    
     [self menu];
+    
+
+    
 }
 
 -(void)optionThree {
@@ -96,41 +106,8 @@
     [self logout];
 }
 
-/*
 
-    
-    NSMutableArray *questions = [[NSMutableArray alloc] init];
- 
-    
-    
-    if ([menuChoice isEqualToString:@"1"])
-    {
-        questions = [NSMutableArray arrayWithArray:genericQuestions];
-        for (NSString *question in questions) {
-            NSLog(@"%@", question);
-            NSString *response = [self requestKeyboardInput];
-            [questionsAndAnswers setObject:response forKey:question];
-        }
 
-        
-        NSLog(@"%@", motherDict);
-        
-    }
-    else if ([menuChoice isEqualToString:@"2"])
-    {
-        NSLog(@"Enter your new question");
-        NSString *inputNewQuestion = [self requestKeyboardInput];
-        [questions addObject:inputNewQuestion];
-        NSLog(@"The array is ..... %@",questions);
-        [self oneLargeMethod];
-    }
-    else if ([menuChoice isEqualToString:@"3"])
-    {
-        NSLog(@"You choose three.");
-    }
-
-}
-*/
 
 // DON'T MODIFY ME
 -(NSString *)requestKeyboardInput {
